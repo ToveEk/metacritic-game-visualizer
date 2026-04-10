0ea6d1d53ca2e36351f36c2ba430219d8fb73250
Tove Ek <te222uu@student.lnu.se>
2026-04-10 13:38:01 +0200

    Add OAuthService for GitHub authentication

---
+++ src/services/oauthService.js
@@ -0,0 +1,46 @@
+export class OAuthService {
+    constructor() {
+        this.clientId = process.env.GITHUB_CLIENT_ID;
+        this.clientSecret = process.env.GITHUB_CLIENT_SECRET;
+        this.redirectUri = process.env.GITHUB_REDIRECT_URI;
+    }
+
+    getAuthUrl() {
+        const state = Math.random().toString(36).substring(2); // Generate a random state parameter to prevent CSRF attacks
+        return `https://github.com/login/oauth/authorize?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&state=${state}`;
+    }
+
+    async exchangeCodeForToken(code) {
+        const response = await fetch('https://github.com/login/oauth/access_token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                client_id: this.clientId,
+                client_secret: this.clientSecret,
+                code: code,
+                redirect_uri: this.redirectUri
+            })
+        });
+        const data = await response.json();
+        return data.access_token;
+
+    }
+
+    async getGitHubUserData(accessToken) {
+        const response = await fetch('https://api.github.com/user', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+        const userData = await response.json();
+        console.log('GitHub user data:', userData);
+        return userData;
+
+    }
+
+    async getApiToken() {
+
+    }
+}
